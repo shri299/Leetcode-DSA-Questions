6c12322367580999dcf576202f7e903ab2f2d218
Srishti <76170660+shri299@users.noreply.github.com>
2024-09-05 23:23:56 +0530

    Time: 2 ms (48.29%) | Memory: 49.8 MB (20.05%) - LeetSync

--- 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
+++ 1498-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.java
@@ -9,32 +9,54 @@
  */
 
 class Solution {
+
+    TreeNode answer = null;
+
+    // public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+    //     if(original==null && cloned==null){
+    //         return null;
+    //     }
+
+    //     Queue<TreeNode> queue = new LinkedList<>();
+    //     queue.offer(cloned);
+
+    //     while(!queue.isEmpty()){
+    //         int levelSize = queue.size();
+
+    //         for(int i=0;i<levelSize;i++){
+    //             TreeNode current = queue.poll();
+    //             if(current.val==target.val){
+    //                 return current;
+    //             }
+    //             if(current.left!=null){
+    //                 queue.offer(current.left);
+    //             }
+
+    //             if(current.right!=null){
+    //                 queue.offer(current.right);
+    //             }
+    //         }
+    //     }
+
+    //     return null;
+    // }
+
     public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
-        if(original==null && cloned==null){
-            return null;
-        }
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.offer(cloned);
-
-        while(!queue.isEmpty()){
-            int levelSize = queue.size();
-
-            for(int i=0;i<levelSize;i++){
-                TreeNode current = queue.poll();
-                if(current.val==target.val){
-                    return current;
-                }
-                if(current.left!=null){
-                    queue.offer(current.left);
-                }
-
-                if(current.right!=null){
-                    queue.offer(current.right);
-                }
-            }
-        }
-
-        return null;
+        targetCopy(cloned,target.val);
+        return answer;
     }
+
+    public final void targetCopy(final TreeNode cloned, int target){
+        if(cloned==null){
+            return;
+        }
+
+        if(cloned.val==target){
+            answer = cloned;
+        }
+
+        targetCopy(cloned.left,target);
+        targetCopy(cloned.right,target);
+    }
+
 }
