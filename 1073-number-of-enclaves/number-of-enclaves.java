44fe90ff3ea53b89905e355b6bbabd60678507f1
Srishti <76170660+shri299@users.noreply.github.com>
2024-09-21 16:02:56 +0530

    Time: 11 ms (19.30%) | Memory: 60.6 MB (18.88%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,49 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int count=0;
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean[][] vis = new boolean[n][m];
+
+        int[] delRow = {-1,0,1,0};
+        int[] delCol = {0,1,0,-1};
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if((i==0 || j==0 || i==n-1 || j==m-1) && grid[i][j]==1){ //check for boundry O's
+                    dfs(i,j,vis,grid,delRow,delCol);
+                }else if(grid[i][j]==0){
+                    vis[i][j]=true;
+                }
+            }
+        }
+
+         for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j]){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public void dfs(int row, int col, boolean[][] vis, int[][] grid, int[] delRow, int[] delCol){
+        vis[row][col] = true;
+        int n = grid.length;
+        int m = grid[0].length;
+
+        //check in four directions
+
+        for(int i=0;i<4;i++){
+            int newRow = row + delRow[i];
+            int newCol = col + delCol[i];
+
+            if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && !vis[newRow][newCol] && grid[newRow][newCol]!=0){
+                dfs(newRow,newCol,vis,grid,delRow,delCol);
+            }
+        }
+    }
+}
