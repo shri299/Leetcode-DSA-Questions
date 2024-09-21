a6b97f4e624e8d5da861d0d1a0f3eddb0b65986a
Srishti <76170660+shri299@users.noreply.github.com>
2024-09-21 16:35:11 +0530

    Time: 11 ms (19.30%) | Memory: 54.9 MB (68.52%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -5,6 +5,7 @@ class Solution {
         int m = grid[0].length;
 
         boolean[][] vis = new boolean[n][m];
+        Queue<Pair> queue = new LinkedList<>();
 
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
@@ -12,11 +13,28 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if((i==0 || j==0 || i==n-1 || j==m-1) && grid[i][j]==1){ //check for boundry O's
-                    dfs(i,j,vis,grid,delRow,delCol);
+                    queue.offer(new Pair(i,j));
+                    vis[i][j]=true;
                 }else if(grid[i][j]==0){
                     vis[i][j]=true;
                 }
             }
+        }
+
+        while(!queue.isEmpty()){
+            int row1 = queue.peek().row;
+            int col1 = queue.peek().col;
+            queue.remove();
+
+            for(int i=0;i<4;i++){
+            int newRow = row1 + delRow[i];
+            int newCol = col1 + delCol[i];
+
+            if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && !vis[newRow][newCol] && grid[newRow][newCol]!=0){
+               queue.offer(new Pair(newRow,newCol));
+               vis[newRow][newCol]=true;
+            }
+        }
         }
 
          for(int i=0;i<n;i++){
@@ -30,20 +48,13 @@ class Solution {
         return count;
     }
 
-    public void dfs(int row, int col, boolean[][] vis, int[][] grid, int[] delRow, int[] delCol){
-        vis[row][col] = true;
-        int n = grid.length;
-        int m = grid[0].length;
+    class Pair {
+        int row;
+        int col;
 
-        //check in four directions
-
-        for(int i=0;i<4;i++){
-            int newRow = row + delRow[i];
-            int newCol = col + delCol[i];
-
-            if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && !vis[newRow][newCol] && grid[newRow][newCol]!=0){
-                dfs(newRow,newCol,vis,grid,delRow,delCol);
-            }
+        Pair (int r, int c){
+            this.row=r;
+            this.col=c;
         }
     }
 }
