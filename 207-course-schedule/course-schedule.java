82b76b512900fa3a419eddd15c807a144642c965
Srishti <76170660+shri299@users.noreply.github.com>
2024-09-30 14:32:12 +0530

    Time: 7 ms (36.90%) | Memory: 44.7 MB (68.32%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,35 +1,39 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
-        List<Integer> ans = new ArrayList<>();
+        //create a adj list from the given prerequisites
+
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList();
+        ArrayList<Integer> topo = new ArrayList<>();
 
         for(int i=0;i<numCourses;i++){
-            adjList.add(new ArrayList<>());
+            adjList.add(new ArrayList());
         }
 
         for(int i=0;i<prerequisites.length;i++){
             adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
 
+        //now use kahns algo to check if cycle exists, if it does return false else true
+
+        Queue<Integer> queue = new LinkedList<>();
         int[] inDegree = new int[adjList.size()];
-        for (int i=0;i<numCourses;i++){
+
+        for(int i=0;i<adjList.size();i++){
             for (int node : adjList.get(i)){
                 inDegree[node]++;
             }
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0;i<numCourses;i++){
+        for(int i=0;i<inDegree.length;i++){
             if (inDegree[i]==0){
                 queue.offer(i);
             }
         }
 
-        while (!queue.isEmpty()){
-            int target = queue.poll();
-            ans.add(target);
-
-            for (int neighbour: adjList.get(target)){
+        while(!queue.isEmpty()){
+            int val = queue.poll();
+            topo.add(val);
+            for(int neighbour: adjList.get(val)){
                 inDegree[neighbour]--;
                 if (inDegree[neighbour]==0) {
                     queue.offer(neighbour);
@@ -37,10 +41,9 @@ class Solution {
             }
         }
 
-        if(ans.size()==numCourses){
+        if(topo.size()==numCourses){
             return true;
         }
         return false;
-        
     }
 }
