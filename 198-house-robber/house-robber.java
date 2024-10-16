b1c956fac6b3bc11e1dab4e52f74a9d184679da1
Srishti <76170660+shri299@users.noreply.github.com>
2024-10-17 00:17:54 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (26.01%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -3,22 +3,43 @@ class Solution {
         int[] dp = new int[nums.length];
         Arrays.fill(dp,-1);
 
-        return solve(dp,nums,nums.length-1);
+        //Tabulation
+
+        // dp[0] = nums[0];
+        // int take = 0;
+        // int notTake = 0;
+
+        // for(int i=1;i<nums.length;i++){
+        //     if(i>1){
+        //         take = take + dp[i-2];
+        //     }
+            
+        //     take = nums[i];
+            
+        //     notTake = dp[i-1];
+
+        //     dp[i] = Math.max(take,notTake);
+        // }
+
+        return solve(dp,nums,0);
+        //return dp[nums.length-1];
     }
 
+    //memoization
+
     private int solve(int[] dp, int[] nums, int len){
-        if(len==0){
+        if(len==nums.length-1){
             return nums[len];
         }
-        if(len<0){
+        if(len>nums.length-1){
             return 0;
         }
         if(dp[len]!=-1){
             return dp[len];
         }
 
-        int m1 = nums[len] + solve(dp,nums,len-2);
-        int m2 = 0 + solve(dp,nums,len-1);
+        int m1 = nums[len] + solve(dp,nums,len+2);
+        int m2 = 0 + solve(dp,nums,len+1);
 
         return dp[len]=Math.max(m1,m2);
     }
