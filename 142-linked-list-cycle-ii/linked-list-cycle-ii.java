965b51b6bcd5d5545048eb55d9d43c171b12797d
Srishti <76170660+shri299@users.noreply.github.com>
2024-11-30 13:19:39 +0530

    Time: 1 ms (28.40%) | Memory: 44.3 MB (72.34%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -1,70 +1,39 @@
 /**
  * Definition for singly-linked list.
  * class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) {
- *         val = x;
- *         next = null;
- *     }
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
  * }
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        // ListNode fast = head;
-        // ListNode slow = head;
-        // boolean flag = false;
-        // while(fast!=null && fast.next!=null){
-        //     fast = fast.next.next;
-        //     slow = slow.next;
-        //     if (fast==slow){
-        //         flag = true;
-        //         break;
-        //     }
-        // }
-
-        // //count length of cycle
-        // if(flag){
-        //     slow = slow.next;
-        // int count = 1;
-        // while (fast!=slow){
-        //     slow = slow.next;
-        //     count = count+1;
-        // }
-
-        // ListNode temp = head;
-        // ListNode temp1 = head;
-        // while(count!=0){
-        //     temp1 = temp1.next;
-        //     count--;
-        // }
-
-        // while(temp!=temp1){
-        //     temp = temp.next;
-        //     temp1 = temp1.next;
-        // }
-        // return temp;
-        // }
-
-        // return null;
-
+        if (head == null) {
+            return null;
+        } else if (head.next == null) {
+            return null;
+        }
         ListNode slow = head;
         ListNode fast = head;
-        
-        while (fast != null && fast.next != null) {
+
+        while (slow != null && fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
-            
+
             if (slow == fast) {
-                ListNode slow2 = head;
-                while (slow2 != slow) {
+                ListNode start = head;
+                while (slow != start) {
                     slow = slow.next;
-                    slow2 = slow2.next;
+                    start = start.next;
                 }
                 return slow;
             }
         }
-        
+        System.out.println(slow.val);
+
         return null;
     }
 }
