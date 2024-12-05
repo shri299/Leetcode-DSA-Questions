7bb53298d36f3963edfca31b36e77af8659c8357
Srishti <76170660+shri299@users.noreply.github.com>
2024-12-05 12:36:06 +0530

    Time: 3 ms (87.76%) | Memory: 46.1 MB (95.50%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,4 +1,35 @@
 class Solution {
+    // public int longestOnes(int[] nums, int k) {
+    //     int left=0;
+    //     int right=0;
+    //     int zeroCount = 0;
+    //     int maxLen = 0;
+
+    //     while(right<nums.length){
+    //         if(nums[right]==0){
+    //             zeroCount++;
+    //         }
+
+    //         if(zeroCount>k){
+    //             if(nums[left]==0){
+    //                 zeroCount--;
+    //             }
+    //             left++;
+    //         }
+
+    //         if(zeroCount<=k){
+    //             int len = right-left+1;
+    //             maxLen = Math.max(maxLen,len);
+    //         }
+
+    //         right++;
+    //     }
+
+    //     return maxLen;
+    // }
+
+    //Approach Two
+
     public int longestOnes(int[] nums, int k) {
         int left=0;
         int right=0;
@@ -9,22 +40,20 @@ class Solution {
             if(nums[right]==0){
                 zeroCount++;
             }
-
-            if(zeroCount>k){
-                if(nums[left]==0){
-                    zeroCount--;
-                }
-                left++;
-            }
-
             if(zeroCount<=k){
                 int len = right-left+1;
                 maxLen = Math.max(maxLen,len);
             }
-
+            if(zeroCount>k){ //condition violated, start shrinking
+                while(zeroCount>k){
+                    if(nums[left]==0){
+                        zeroCount--;
+                    }
+                    left++;
+                }
+            }
             right++;
         }
-
         return maxLen;
     }
 }
