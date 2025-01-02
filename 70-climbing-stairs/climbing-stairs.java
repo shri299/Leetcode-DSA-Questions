90db63f77d98d84351c382116f506cf3a07896d2
Srishti <76170660+shri299@users.noreply.github.com>
2025-01-02 21:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (7.59%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,7 +1,6 @@
 class Solution {
     public int climbStairs(int n) {
         //Tabulation with O(1) SC
-
         int prev1 = 1;
         int prev2 = 1;
 
@@ -15,43 +14,4 @@ class Solution {
         return prev2;
 
     }
-
-    //recursion with exponetial TC
-
-    // private int solve(int n){
-    //     if(n<=1){
-    //         return 1;
-    //     }
-
-    //     return solve(n-1) + solve(n-2);
-    // }
-
-
-    //memoization with non-exponential TC
-
-    // private int solve(int n,int[] dp){
-    //     if(n<=1){
-    //         return 1;
-    //     }
-
-    //     if(dp[n]!=-1){
-    //         return dp[n];
-    //     }
-
-    //     return dp[n] = solve(n-1,dp) + solve(n-2,dp);
-    // }
-
-
-    //tabulation with O(n) SC
-
-    // private void solve(int n){
-    //     int[] dp = new int[n + 1];
-    //     dp[0] = 1;
-    //     dp[1] = 1;
-
-    //     for (int i = 2; i <= n; ++i)
-    //     dp[i] = dp[i - 1] + dp[i - 2];
-
-    //     return dp[n];
-    // }
 }
