6c4d5038887d1b20f3b836cfdc9dc2f3e3846f3a
Srishti <76170660+shri299@users.noreply.github.com>
2025-02-22 16:13:09 +0530

    Time: 2 ms (32.26%) | Memory: 43.9 MB (12.07%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,15 +1,21 @@
 class Solution {
+
+    //TC : O(2^n x nlogn){recursion + sorting} + O(2^n x n){adding in the set in worst case}
+    //SC : O(2^n)
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Set<List<Integer>> ans = new HashSet<>();
+        Arrays.sort(nums);
         helper(0,nums,ans,new ArrayList<>());
         return new ArrayList<>(ans);
     }
 
     private void helper(int index,int[] nums,Set<List<Integer>> ans,List<Integer> ds){
         if(index>=nums.length){
-            List<Integer> sortedList = new ArrayList<>(ds);
-            Collections.sort(sortedList);
-            ans.add(sortedList);
+            // List<Integer> sortedList = new ArrayList<>(ds);
+            // Collections.sort(sortedList);
+            // ans.add(sortedList);
+            ans.add(new ArrayList<>(ds));
             return;
         }
 
