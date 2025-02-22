5a7b6d28ea08888175fc1aa414e1c893ee9f3416
Srishti <76170660+shri299@users.noreply.github.com>
2025-02-22 16:31:31 +0530

    Time: 1 ms (99.80%) | Memory: 43.7 MB (44.51%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,27 +1,47 @@
 class Solution {
 
-    //TC : O(2^n x nlogn){recursion + sorting} + O(2^n x n){adding in the set in worst case}
-    //SC : O(2^n)
+    //TC : O(2^n x n){recursion + adding to array} + O(nlogn){sorting the array}
+    //SC : O(2^n) + O(n) {stack space}
 
-    public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Set<List<Integer>> ans = new HashSet<>();
+    // public List<List<Integer>> subsetsWithDup(int[] nums) {
+    //     Set<List<Integer>> ans = new HashSet<>();
+    //     Arrays.sort(nums);
+    //     helper(0,nums,ans,new ArrayList<>());
+    //     return new ArrayList<>(ans);
+    // }
+
+    // private void helper(int index,int[] nums,Set<List<Integer>> ans,List<Integer> ds){
+    //     if(index>=nums.length){
+    //         ans.add(new ArrayList<>(ds));
+    //         return;
+    //     }
+
+    //     ds.add(nums[index]);
+    //     helper(index+1,nums,ans,ds);
+    //     ds.remove(ds.size()-1);
+    //     helper(index+1,nums,ans,ds);
+    // }
+
+
+    //Approach 2 : 
+
+    public List<List<Integer>> subsetsWithDup(int[] nums){
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
         helper(0,nums,ans,new ArrayList<>());
-        return new ArrayList<>(ans);
+        return ans;
     }
 
-    private void helper(int index,int[] nums,Set<List<Integer>> ans,List<Integer> ds){
-        if(index>=nums.length){
-            // List<Integer> sortedList = new ArrayList<>(ds);
-            // Collections.sort(sortedList);
-            // ans.add(sortedList);
-            ans.add(new ArrayList<>(ds));
-            return;
-        }
+    private void helper(int index,int[] nums,List<List<Integer>> ans,List<Integer> ds){
+         ans.add(new ArrayList<>(ds));
 
-        ds.add(nums[index]);
-        helper(index+1,nums,ans,ds);
-        ds.remove(ds.size()-1);
-        helper(index+1,nums,ans,ds);
+         for(int i=index;i<nums.length;i++){
+            if(i!=index && nums[i]==nums[i-1]){
+                continue;
+            }
+            ds.add(nums[i]);
+            helper(i+1,nums,ans,ds);
+            ds.remove(ds.size()-1);
+         }
     }
 }
