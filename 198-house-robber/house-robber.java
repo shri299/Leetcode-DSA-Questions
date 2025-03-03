20eac4769062e89db5b34b9f5aa9fb71b3e1d512
Srishti <76170660+shri299@users.noreply.github.com>
2025-03-04 00:16:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (97.90%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,28 +1,46 @@
 class Solution {
     public int rob(int[] nums) {
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp,-1);
+        // int[] dp = new int[nums.length];
+        // Arrays.fill(dp,-1);
         
         //Tabulation
-        int max = Integer.MIN_VALUE;
-        if(nums.length==1) return nums[0];
-        for(int i=0;i<nums.length;i++){
-            int pick = 0;
-            if(i-2<0){
-                pick = nums[i];
-            }else{
-                pick = nums[i] + dp[i-2];
-            }
 
-            int notPick = 0;
-            if(i-1>=0){
-                notPick = dp[i-1];
-            }
-            max = Math.max(pick,notPick);
-            dp[i] = max;
+        // int max = Integer.MIN_VALUE;
+        // if(nums.length==1) return nums[0];
+        // for(int i=0;i<nums.length;i++){
+        //     int pick = 0;
+        //     if(i-2<0){
+        //         pick = nums[i];
+        //     }else{
+        //         pick = nums[i] + dp[i-2];
+        //     }
+
+        //     int notPick = 0;
+        //     if(i-1>=0){
+        //         notPick = dp[i-1];
+        //     }
+        //     max = Math.max(pick,notPick);
+        //     dp[i] = max;
+        // }
+
+        // return max;
+
+        //Space optimization
+
+        int prev1 = nums[0];
+        int prev2 = 0;
+
+        for(int i=1;i<nums.length;i++){
+            int pick = nums[i];
+            if(i>1) pick += prev2;
+
+            int notPick = 0 + prev1;
+
+            prev2 = prev1;
+            prev1 = Math.max(pick,notPick);
         }
 
-        return max;
+        return prev1;
     }
 
     //Recursion : O(2^n)
