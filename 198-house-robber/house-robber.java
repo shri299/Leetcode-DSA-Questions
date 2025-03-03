6919577c109a81dfaa36d21940823f93cc86dc09
Srishti <76170660+shri299@users.noreply.github.com>
2025-03-04 00:09:19 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (58.99%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -2,10 +2,31 @@ class Solution {
     public int rob(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp,-1);
-        return helper(nums,nums.length-1,dp);
+        
+        //Tabulation
+        int max = Integer.MIN_VALUE;
+        if(nums.length==1) return nums[0];
+        for(int i=0;i<nums.length;i++){
+            int pick = 0;
+            if(i-2<0){
+                pick = nums[i];
+            }else{
+                pick = nums[i] + dp[i-2];
+            }
+
+            int notPick = 0;
+            if(i-1>=0){
+                notPick = dp[i-1];
+            }
+            max = Math.max(pick,notPick);
+            dp[i] = max;
+        }
+
+        return max;
     }
 
     //Recursion : O(2^n)
+
     // private int helper(int[] nums,int index){
     //     if(index==0){
     //         return nums[index];
@@ -21,27 +42,29 @@ class Solution {
     // }
 
 
-    private int helper(int[] nums,int index, int[] dp){
-        if(index==0){
-            return nums[index];
-        }
-        if(index<0){
-            return 0;
-        }
-        int pick = 0;
-        if(index-2>0 && dp[index-2]!=-1){
-            pick = nums[index] + dp[index-2];
-        }else{
-            pick = nums[index] + helper(nums,index-2,dp);
-        }
+    //Memoization
 
-        int notPick = 0;
-        if(dp[index-1]!=-1){
-            notPick = dp[index-1];
-        }else{
-            notPick = helper(nums,index-1,dp);
-        }
+    // private int helper(int[] nums,int index, int[] dp){
+    //     if(index==0){
+    //         return nums[index];
+    //     }
+    //     if(index<0){
+    //         return 0;
+    //     }
+    //     int pick = 0;
+    //     if(index-2>0 && dp[index-2]!=-1){
+    //         pick = nums[index] + dp[index-2];
+    //     }else{
+    //         pick = nums[index] + helper(nums,index-2,dp);
+    //     }
 
-        return dp[index] = Math.max(pick,notPick);
-    }
+    //     int notPick = 0;
+    //     if(dp[index-1]!=-1){
+    //         notPick = dp[index-1];
+    //     }else{
+    //         notPick = helper(nums,index-1,dp);
+    //     }
+
+    //     return dp[index] = Math.max(pick,notPick);
+    // }
 }
