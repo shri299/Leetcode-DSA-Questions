ccc3e8a3d147b6b382d203cedf04def21a619185
Srishti <76170660+shri299@users.noreply.github.com>
2025-03-25 21:09:14 +0530

    Time: 1 ms (88.99%) | Memory: 48.3 MB (21.20%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,7 +1,7 @@
 public class Solution {
     public void dfs(int[][] M, int[] visited, int i) {
         for (int j = 0; j < M.length; j++) {
-            if (M[i][j] == 1 && visited[j] == 0) {
+            if (M[i][j] == 1 && visited[j] == 0 && i!=j) {
                 visited[j] = 1;
                 dfs(M, visited, j);
             }
