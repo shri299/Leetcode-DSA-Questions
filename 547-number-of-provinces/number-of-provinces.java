a6fcea758a1609881518f68b589722b8c89de31c
Srishti <76170660+shri299@users.noreply.github.com>
2025-03-25 21:08:40 +0530

    Time: 1 ms (88.99%) | Memory: 48.2 MB (28.86%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,46 +1,21 @@
-class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        //convert adj matrix to adj list
-        ArrayList<ArrayList<Integer>> nodeList = new ArrayList<ArrayList<Integer>>();
-        int len = isConnected.length;
-        for(int i=0;i<=len;i++)
-        {
-            nodeList.add(new ArrayList<>());
-        }
-        for(int i=0;i<len;i++){
-            for(int j=0;j<len;j++){
-                if(isConnected[i][j]==1 && i!=j)
-                {
-                    nodeList.get(i+1).add(j+1);
-                    nodeList.get(j+1).add(i+1);
-                }
+public class Solution {
+    public void dfs(int[][] M, int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
             }
         }
-
-        return traversal(len,nodeList);
-
-
     }
-
-    public static int traversal(int totalNodes, ArrayList<ArrayList<Integer>> adjList){
+    public int findCircleNum(int[][] M) {
+        int[] visited = new int[M.length];
         int count = 0;
-        boolean[] visitedArray = new boolean[totalNodes+1];
-        visitedArray[0] = true; //we have nothing to do with this index
-        for(int i=0;i<visitedArray.length;i++){
-            if (!visitedArray[i]){
-                traverse(visitedArray,adjList,i);
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                dfs(M, visited, i);
                 count++;
             }
         }
         return count;
-    }
-
-    private static void traverse(boolean[] visitedArray, ArrayList<ArrayList<Integer>> adjList, int node) {
-        visitedArray[node]=true;
-        for (Integer neighbourNodes : adjList.get(node)){
-            if (!visitedArray[neighbourNodes]){
-                traverse(visitedArray,adjList,neighbourNodes);
-            }
-        }
     }
 }
