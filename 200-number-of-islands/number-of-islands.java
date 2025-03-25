e44e5bb493d5b2bb2795010eeb14e394fd2288ed
Srishti <76170660+shri299@users.noreply.github.com>
2025-03-25 21:32:33 +0530

    Time: 4 ms (50.55%) | Memory: 49.3 MB (84.99%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,77 +1,32 @@
 class Solution {
+    public int[] delI = {-1,0,1,0};
+    public int[] delJ = {0,1,0,-1};
     public int numIslands(char[][] grid) {
-        //create a dummy visited array
-        int n = grid.length;
-        int m = grid[0].length;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        boolean[][] vis = new boolean[rows][cols];
+        int count = 0;
 
-        boolean[][] vis = new boolean[n][m];
-
-        int count = 0;  //placeholder for the answer
-
-        //iterating the entire grid
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]!='0'){
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++){
+                if(grid[i][j]=='1' && !vis[i][j]){
                     count++;
-                    bfs(i,j,vis,grid);
+                    dfs(vis,grid,i,j,rows,cols);
                 }
             }
         }
-        
-
         return count;
     }
 
-    public void bfs(int r, int c, boolean[][] vis, char[][] grid){
-        Queue<Pair> queue = new LinkedList<>(); //queue for bfs
-        vis[r][c] = true;
-        queue.offer(new Pair(r,c));
-
-        //traverse the neigbours
-
-        while(!queue.isEmpty()){
-            int row = queue.peek().first;
-            int col = queue.peek().second;
-            queue.remove();
-
-            //4 directions -> 4 cases
-
-            if(validate(row-1,col,vis,grid)){
-                queue.offer(new Pair(row-1,col));
-                vis[row-1][col] = true;
+    private void dfs(boolean[][] vis, char[][] grid,int row, int col, int rowLen, int colLen){
+        vis[row][col]=true;
+        for(int i=0;i<4;i++){
+            int newRow = row + delI[i];
+            int newCol = col + delJ[i];
+            if(newRow>=0 && newRow<rowLen && newCol>=0 && newCol<colLen && !vis[newRow][newCol] && grid[newRow][newCol]=='1'){
+                dfs(vis,grid,newRow,newCol,rowLen,colLen);
             }
-
-            if(validate(row+1,col,vis,grid)){
-                queue.offer(new Pair(row+1,col));
-                vis[row+1][col] = true;
-            }
-
-            if(validate(row,col+1,vis,grid)){
-                queue.offer(new Pair(row,col+1));
-                vis[row][col+1] = true;
-            }
-
-            if(validate(row,col-1,vis,grid)){
-                queue.offer(new Pair(row,col-1));
-                vis[row][col-1] = true;
-            }
-        }
-        
-    }
-
-    public boolean validate(int row, int col, boolean[][] vis, char[][] grid){
-        int n = grid.length;
-        int m = grid[0].length;
-        return row>=0 && row<n && col>=0 && col<m && !vis[row][col] && grid[row][col]!='0';
-    }
-
-    class Pair{
-        int first;
-        int second;
-
-        Pair (int first, int second){
-            this.first=first;
-            this.second=second;
         }
     }
 }
