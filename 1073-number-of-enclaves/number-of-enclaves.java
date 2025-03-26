ef982dc1cf7936b1b58c0b0331634d600ef9752c
Srishti <76170660+shri299@users.noreply.github.com>
2025-03-26 23:08:01 +0530

    Time: 11 ms (19.40%) | Memory: 60 MB (26.04%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,11 +1,9 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int count=0;
-        int n = grid.length;
+         int n = grid.length;
         int m = grid[0].length;
-
+        int count = 0;
         boolean[][] vis = new boolean[n][m];
-        Queue<Pair> queue = new LinkedList<>();
 
         int[] delRow = {-1,0,1,0};
         int[] delCol = {0,1,0,-1};
@@ -13,28 +11,11 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if((i==0 || j==0 || i==n-1 || j==m-1) && grid[i][j]==1){ //check for boundry O's
-                    queue.offer(new Pair(i,j));
-                    vis[i][j]=true;
+                    dfs(i,j,vis,grid,delRow,delCol);
                 }else if(grid[i][j]==0){
                     vis[i][j]=true;
                 }
             }
-        }
-
-        while(!queue.isEmpty()){
-            int row1 = queue.peek().row;
-            int col1 = queue.peek().col;
-            queue.remove();
-
-            for(int i=0;i<4;i++){
-            int newRow = row1 + delRow[i];
-            int newCol = col1 + delCol[i];
-
-            if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && !vis[newRow][newCol] && grid[newRow][newCol]!=0){
-               queue.offer(new Pair(newRow,newCol));
-               vis[newRow][newCol]=true;
-            }
-        }
         }
 
          for(int i=0;i<n;i++){
@@ -48,13 +29,20 @@ class Solution {
         return count;
     }
 
-    class Pair {
-        int row;
-        int col;
+    public void dfs(int row, int col, boolean[][] vis, int[][] board, int[] delRow, int[] delCol){
+        vis[row][col] = true;
+        int n = board.length;
+        int m = board[0].length;
 
-        Pair (int r, int c){
-            this.row=r;
-            this.col=c;
+        //check in four directions
+
+        for(int i=0;i<4;i++){
+            int newRow = row + delRow[i];
+            int newCol = col + delCol[i];
+
+            if(newRow>=0 && newRow<n && newCol>=0 && newCol<m && !vis[newRow][newCol] && board[newRow][newCol]!=0){
+                dfs(newRow,newCol,vis,board,delRow,delCol);
+            }
         }
     }
 }
