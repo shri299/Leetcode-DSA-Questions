ef9a1f8c1b82d41d54b4e404dc4fa3ccea0290ff
Srishti <76170660+shri299@users.noreply.github.com>
2025-05-03 23:43:04 +0530

    Time: 3 ms (91.46%) | Memory: 46.3 MB (81.74%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -1,59 +1,59 @@
 class Solution {
-    // public int longestOnes(int[] nums, int k) {
-    //     int left=0;
-    //     int right=0;
-    //     int zeroCount = 0;
-    //     int maxLen = 0;
-
-    //     while(right<nums.length){
-    //         if(nums[right]==0){
-    //             zeroCount++;
-    //         }
-
-    //         if(zeroCount>k){
-    //             if(nums[left]==0){
-    //                 zeroCount--;
-    //             }
-    //             left++;
-    //         }
-
-    //         if(zeroCount<=k){
-    //             int len = right-left+1;
-    //             maxLen = Math.max(maxLen,len);
-    //         }
-
-    //         right++;
-    //     }
-
-    //     return maxLen;
-    // }
-
-    //Approach Two
-
     public int longestOnes(int[] nums, int k) {
-        int left = 0;
-        int right = 0;
+        int left=0;
+        int right=0;
         int zeroCount = 0;
         int maxLen = 0;
 
-        while (right < nums.length) {
-            if (nums[right] == 0) {
+        while(right<nums.length){
+            if(nums[right]==0){
                 zeroCount++;
             }
-            if (zeroCount <= k) {
-                int len = right - left + 1;
-                maxLen = Math.max(maxLen, len);
-            }
-            if(zeroCount>k){ //condition violated, start shrinking
-                while(zeroCount>k){
-                    if(nums[left]==0){
-                        zeroCount--;
-                    }
-                    left++;
+
+            if(zeroCount>k){
+                if(nums[left]==0){
+                    zeroCount--;
                 }
+                left++;
             }
+
+            if(zeroCount<=k){
+                int len = right-left+1;
+                maxLen = Math.max(maxLen,len);
+            }
+
             right++;
         }
+
         return maxLen;
     }
+
+    //Approach Two
+
+    // public int longestOnes(int[] nums, int k) {
+    //     int left = 0;
+    //     int right = 0;
+    //     int zeroCount = 0;
+    //     int maxLen = 0;
+
+    //     while (right < nums.length) {
+    //         if (nums[right] == 0) {
+    //             zeroCount++;
+    //         }
+    //         if (zeroCount <= k) {
+    //             int len = right - left + 1;
+    //             maxLen = Math.max(maxLen, len);
+    //         }
+    //         if(zeroCount>k){ //condition violated, start shrinking
+    //             while(zeroCount>k){
+    //                 if(nums[left]==0){
+    //                     zeroCount--;
+    //                 }
+    //                 left++;
+    //             }
+    //         }
+    //         right++;
+    //     }
+    //     return maxLen;
+    // }
 }
