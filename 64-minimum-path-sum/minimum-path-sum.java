3b2340ad19b0e1037dfaf6319253ab28f13e6e82
Srishti <76170660+shri299@users.noreply.github.com>
2025-06-02 00:01:40 +0530

    Time: 4 ms (62.41%) | Memory: 47.6 MB (69.95%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -18,27 +18,58 @@ class Solution {
     // }
 
 
+    // public int minPathSum(int[][] grid) {
+    //     int n = grid.length;
+    //     int m = grid[0].length;
+    //     int[][] dp = new int[n][m];
+        
+    //     for(int i=0;i<n;i++){
+    //         for(int j=0;j<m;j++){
+    //             if(i==0 && j==0){
+    //                 dp[i][j]=grid[i][j];
+    //             }else{
+    //                 if(i-1<0){
+    //                     //current row's previous col
+    //                     dp[i][j] = grid[i][j] + dp[i][j-1];
+    //                 }else if(j-1<0){
+    //                     //previous row's current col
+    //                     dp[i][j] = grid[i][j] + dp[i-1][j];
+    //                 }else{
+    //                     dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     return dp[n-1][m-1];
+    // }
+
+
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int[][] dp = new int[n][m];
+        int[] prev = new int[m];
         
         for(int i=0;i<n;i++){
+            int[] current = new int[m];
             for(int j=0;j<m;j++){
                 if(i==0 && j==0){
-                    dp[i][j]=grid[i][j];
+                    current[j]=grid[i][j];
                 }else{
                     if(i-1<0){
-                        dp[i][j] = grid[i][j] + dp[i][j-1];
+                        //current row's previous col
+                        current[j] = grid[i][j] + current[j-1];
                     }else if(j-1<0){
-                        dp[i][j] = grid[i][j] + dp[i-1][j];
+                        //previous row's current col
+                        current[j] = grid[i][j] + prev[j];
                     }else{
-                        dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+                        current[j] = grid[i][j] + Math.min(prev[j],current[j-1]);
                     }
                 }
             }
+            prev = current;
         }
 
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 }
