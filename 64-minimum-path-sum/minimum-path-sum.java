2cf22150da66152a14aa9e454a76a52882dd45d0
Srishti <76170660+shri299@users.noreply.github.com>
2025-06-01 23:54:47 +0530

    Time: 5 ms (29.03%) | Memory: 48.2 MB (8.48%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,19 +1,44 @@
 class Solution {
+    // public int minPathSum(int[][] grid) {
+    //     int n = grid.length;
+    //     int m = grid[0].length;
+    //     int[][] dp = new int[n][m];
+    //     Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
+    //     return helper(n-1,m-1,grid,dp);
+    // }
+
+    // private int helper(int row, int col, int[][] grid, int[][] dp){
+    //     if(row==0 && col==0) return grid[row][col];
+    //     if(row<0 || col<0) return (int)1e9;
+    //     if(dp[row][col]!=-1) return dp[row][col];
+    //     int up = grid[row][col] + helper(row-1,col,grid,dp);
+    //     int left = grid[row][col] + helper(row,col-1,grid,dp);
+
+    //     return dp[row][col] = Math.min(up,left);
+    // }
+
+
     public int minPathSum(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
         int[][] dp = new int[n][m];
-        Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
-        return helper(n-1,m-1,grid,dp);
-    }
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j==0){
+                    dp[i][j]=grid[i][j];
+                }else{
+                    if(i-1<0){
+                        dp[i][j] = grid[i][j] + dp[i][j-1];
+                    }else if(j-1<0){
+                        dp[i][j] = grid[i][j] + dp[i-1][j];
+                    }else{
+                        dp[i][j] = grid[i][j] + Math.min(dp[i-1][j],dp[i][j-1]);
+                    }
+                }
+            }
+        }
 
-    private int helper(int row, int col, int[][] grid, int[][] dp){
-        if(row==0 && col==0) return grid[row][col];
-        if(row<0 || col<0) return (int)1e9;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int up = grid[row][col] + helper(row-1,col,grid,dp);
-        int left = grid[row][col] + helper(row,col-1,grid,dp);
-
-        return dp[row][col] = Math.min(up,left);
+        return dp[n-1][m-1];
     }
 }
