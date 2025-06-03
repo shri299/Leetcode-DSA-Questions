33731695e6af64d216145e2cdc4ab0c9e1532ccb
Srishti <76170660+shri299@users.noreply.github.com>
2025-06-03 17:19:24 +0530

    Time: 4 ms (41.81%) | Memory: 44.1 MB (77.41%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,19 +1,43 @@
 class Solution {
+
+    //Memoization
+    //TC:rowxcol
+    //SC: dp + stack space
+    // public int minimumTotal(List<List<Integer>> triangle) {
+    //     int[][] dp = new int[triangle.size()][triangle.size()];
+    //     Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
+    //     return helper(0,0,triangle,dp);
+    // }
+
+    // private int helper(int row, int col, List<List<Integer>> triangle,int[][] dp){
+    //     if(row==triangle.size()-1) return triangle.get(row).get(col);
+
+    //     if(dp[row][col]!=-1){
+    //         return dp[row][col];
+    //     }
+    //     int down = triangle.get(row).get(col) + helper(row+1,col,triangle,dp);
+    //     int diagonal = triangle.get(row).get(col) + helper(row+1,col+1,triangle,dp);
+
+    //     return dp[row][col]=Math.min(down,diagonal);
+    // }
+
+
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] dp = new int[triangle.size()][triangle.size()];
-        Arrays.stream(dp).forEach(row -> Arrays.fill(row, -1));
-        return helper(0,0,triangle,dp);
-    }
-
-    private int helper(int row, int col, List<List<Integer>> triangle,int[][] dp){
-        if(row==triangle.size()-1) return triangle.get(row).get(col);
-
-        if(dp[row][col]!=-1){
-            return dp[row][col];
+        //start from base and go to top
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=triangle.get(n-1).get(i);
         }
-        int down = triangle.get(row).get(col) + helper(row+1,col,triangle,dp);
-        int diagonal = triangle.get(row).get(col) + helper(row+1,col+1,triangle,dp);
 
-        return dp[row][col]=Math.min(down,diagonal);
+        for(int i = n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down = triangle.get(i).get(j) + dp[i+1][j];
+                int diagonal = triangle.get(i).get(j) + dp[i+1][j+1];
+                dp[i][j] = Math.min(down,diagonal);
+            }
+        }
+
+        return dp[0][0];
     }
 }
